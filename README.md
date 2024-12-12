f7ca1132e48edd176e61fc28099bb9fe74e11364
Nikola Spiric <ext.nikola.spiric@truedigital.com>
2024-12-12 21:12:24 +0100

    Disable desktop environment to speed up playback

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-
+Install service
 ```
 sudo ./install.sh <USERNAME> <GROUP_NAME>
 ```
 
+Test
+```
+systemctl start pi_fireplace.service
+```
+
+Service will auto start on boot
--- install.sh
+++ install.sh
@@ -4,24 +4,24 @@ main() {
     local user=$1
     local group=$2
 
-    set -e
+    set -ex
 
-    echo "Installing files .."
     mkdir -p /usr/bin/pi_fireplace
     cp -v start.sh fireplace.mp4 /usr/bin/pi_fireplace/
     chmod a+x /usr/bin/pi_fireplace/start.sh
 
-    echo "Installing service .."
     cp -v pi_fireplace.service /lib/systemd/system/
 
-    echo "User=$user" >> /lib/systemd/system/pi_fireplace.service
-    echo "Group=$group" >> /lib/systemd/system/pi_fireplace.service
+    echo "User=$user" >>/lib/systemd/system/pi_fireplace.service
+    echo "Group=$group" >>/lib/systemd/system/pi_fireplace.service
 
     systemctl daemon-reload
     systemctl enable pi_fireplace.service
 
-    echo "Done, run:"
-    echo "systemctl start pi_fireplace.service"
+    # switch to a lightweight window manager or run the video in a console without a desktop environment
+    systemctl disable lightdm
+
+    echo "done"
 }
 
 main "$@"
