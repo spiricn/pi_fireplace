5bcc13868486d5f0d14d508918884c24ab8f7573
Nikola Spiric <ext.nikola.spiric@truedigital.com>
2024-12-12 22:04:24 +0100

    Add retry

--- start.sh
+++ start.sh
@@ -5,13 +5,22 @@ main() {
 
     export DISPLAY=:0
 
-    cvlc \
-        --fullscreen \
-        --video-on-top \
-        --repeat \
-        --no-video-title \
-        --no-audio \
-        $scriptDir/fireplace.mp4
+    while true; do
+        cvlc \
+            --fullscreen \
+            --video-on-top \
+            --repeat \
+            --no-video-title \
+            --no-audio \
+            "$scriptDir/fireplace.mp4"
+        
+        if [ $? -eq 0 ]; then
+            echo "Script executed successfully."
+            break
+        else
+            echo "Script failed. Retrying in 5 seconds..."
+            sleep 5
+        fi
 }
 
 main "$@"
